cbdb90c0cd9f52e15cb416cf53432a6fbaa765e9
Taylor <thulsmans.2994@me.com>
2018-01-18 01:45:53 +0000

    sales tax

--- w1d3/sales-tax.js
+++ w1d3/sales-tax.js
@@ -4,20 +4,94 @@ var salesTaxRates = {
   SK: 0.10
 };
 
-var companySalesData = [
-  {
+var companySalesData = [{
     name: "Telus",
     province: "BC",
-    sales: [ 100, 200, 400 ]
+    sales: [100, 200, 400]
   },
   {
     name: "Bombardier",
     province: "AB",
-    sales: [ 80, 20, 10, 100, 90, 500 ]
+    sales: [80, 20, 10, 100, 90, 500]
   },
   {
     name: "Telus",
     province: "SK",
-    sales: [ 500, 100 ]
+    sales: [500, 100]
   }
 ];
+
+function companyGenerator(salesData) {
+  var output = {};
+  salesData.forEach(function(entry) {
+    if (!output.hasOwnProperty(entry['name'])) {
+      output[entry['name']] = {};
+    }
+
+
+  });
+  return output
+}
+
+function totalSales(salesData, company) {
+  var output = 0;
+
+  salesData.forEach(function(entry) {
+    if (entry['name'] == company) {
+      var salesArray = entry['sales'];
+      salesArray.forEach(function(revenue) {
+        output += revenue;
+      })
+    }
+  });
+  return output
+}
+
+function calculateTax(salesData, taxRates, company) {
+  var output = 0;
+  salesData.forEach(function(entry) {
+    if (entry['name'] == company) {
+      var salesArray = entry['sales']
+      console.log(salesArray)
+      var provSales = salesArray.reduce(function(a, b) {
+        return a + b;
+      }, 0);
+      for (rate in taxRates) {
+        if (rate == entry['province']) {
+          output += taxRates[rate] * provSales;
+        }
+      }
+    }
+  })
+  return output
+}
+
+
+
+function calculateSalesTax(salesData, taxRates) {
+  var output = companyGenerator(salesData)
+  for (company in output) {
+    console.log(company)
+    output[company]['totalSales'] = totalSales(salesData, company);
+    output[company]['totalTaxes'] = calculateTax(salesData, taxRates, company)
+  }
+  return output
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+console.log(results);
+
+
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
