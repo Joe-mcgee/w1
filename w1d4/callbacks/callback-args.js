071d86da064049fd967863e105231a8f5344cbe5
Taylor <thulsmans.2994@me.com>
2018-01-18 19:39:43 +0000

    refactor

--- w1d4/callbacks/callback-args.js
+++ w1d4/callbacks/callback-args.js
@@ -1,11 +1,14 @@
 // The second argument/parameter is expected to be a function
+
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function (item, i) {
+    if (item === 'Waldo') {
+      found(i)
     }
-  }
+  })
+
 }
+
 
 function actionWhenFound(i) {
 
@@ -13,3 +16,5 @@ function actionWhenFound(i) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
